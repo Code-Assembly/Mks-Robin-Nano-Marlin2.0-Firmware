814068eaf908acd80972eb3f944c7f33b0a0aae0
Aadil Cachalia <aadil.cachalia@camcog.com>
2022-01-20 18:18:23 +0200

    Adjust default offsets and tramming points

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -932,7 +932,7 @@
 #if ENABLED(ASSISTED_TRAMMING)
 
   // Define positions for probe points.
-  #define TRAMMING_POINT_XY { {  20, 20 }, { 180,  20 }, { 180, 180 }, { 20, 180 }}
+  #define TRAMMING_POINT_XY { {  35, 35 }, { 256,  35 }, { 265, 265 }, { 35, 265 }}
 
   // Define position names for probe points.
   #define TRAMMING_POINT_NAME_1 "Front-Left"
